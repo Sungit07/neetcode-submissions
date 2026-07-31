0433df7a37e957d2bd9e7270ba2b9b2fb47d5ea3
Sungit07 <somyarajsinghdalawat07@gmail.com>
2026-07-31 15:45:00 +0530

    Add: buy-and-sell-crypto-with-cooldown - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n = prices.size();
+
+        if(n==1)
+            return 0;
+
+        vector<vector<int>> dp(n, vector<int>(2));
+
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+
+        dp[1][0] = max(-prices[0], -prices[1]);
+        dp[1][1] = max(0, prices[1]-prices[0]);
+
+        for(int i=2;i<n;i++){
+
+            dp[i][0]=max(dp[i-1][0],
+                         dp[i-2][1]-prices[i]);
+
+            dp[i][1]=max(dp[i-1][1],
+                         dp[i-1][0]+prices[i]);
+        }
+
+        return dp[n-1][1];
+    }
+};
